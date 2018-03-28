865962ffefa55f55a95ed0af0af1852fb3e34c4e
Oren WF <ofriedman@acm.org>
2018-03-28 00:04:37 -0400

    added form view template class login.html and login() function to routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username' : 'Oren'}
+    user = {'username': 'Oren'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -16,3 +18,13 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'
+            .format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
