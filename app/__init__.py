3c57271f584a9da07a1af53851ffc1c93a464dcd
Oren WF <ofriedman@acm.org>
2018-04-03 02:51:20 -0400

    added bootstrap styling to templates

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -11,5 +12,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
+
 
 from app import routes, models
