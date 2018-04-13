1e6cbd734c7ce6d90a6e3e48832428e6c3f3313d
OrenWF <ofriedman@acm.org>
2018-04-13 19:54:55 -0400

    improvements to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,33 @@
 #!/usr/bin/env sh
 
+# https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.6 python3.6-venv
+
+if [ ! -d "venv" ]
+then
+	python3.6 -m venv venv
+else
+	echo 'venv already exists'
+fi
+
+sudo apt install git
+
+if [ ! -d "covermonkey" ]
+then
+	git clone https://github.com/orenwf/covermonkey.git
+else
+	echo 'covermonkey already exists'
+fi
+
+source venv/bin/activate
+cd covermonkey
+pip install -r requirements.txt
+
 TITLE='[CoverMonkey Setup]:'
 
-export FLASK_APP=covermonkey.py
+export FLASK_APP='covermonkey.py'
 echo "$TITLE set FLASK_APP=$FLASK_APP"
 
 if [ ! -e "app.db" ] && [ ! -d "migrations" ]
@@ -13,3 +38,6 @@ then
 else
 	echo "$TITLE db file already exists"
 fi
+
+pip install gunicorn
+gunicorn -b localhost:8000 covermonkey:app
