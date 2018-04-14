33ec0a2cdafdcb528732d8be3b50868f50ebd9b1
OrenWF <ofriedman@acm.org>
2018-04-13 22:22:29 -0400

    edit to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
 
+TITLE='[CoverMonkey Setup]:'
 MYPYTHON=python3
 
 if lsb_release -d | grep -q 'Ubuntu.*16.04' 
 then
+	MYPYTHON=python3.6
+	echo "$TITLE installing Python3.6 and pip"
 	sudo add-apt-repository ppa:deadsnakes/ppa
 	sudo apt-get update
 	sudo apt-get install python3.6
-	MYPYTHON=python3.6
 	sudo apt-get install python3-pip
+else
+	echo 'Not on Ubuntu 16.04'
 fi
 
 if [ ! -d "venv" ]
@@ -18,13 +22,10 @@ else
 	echo 'venv already exists'
 fi
 
-pip install -U pip
 source venv/bin/activate
+pip install -U pip
 cd covermonkey
 pip install -r requirements.txt
-pip install gunicorn
-
-TITLE='[CoverMonkey Setup]:'
 
 export FLASK_APP='covermonkey.py'
 echo "$TITLE set FLASK_APP=$FLASK_APP"
@@ -35,9 +36,9 @@ then
 	flask db migrate -m "initial"
 	flask db upgrade
 else
-	echo "$TITLE db file already exists"
+	echo "$TITLE db file and migrations directory already exist"
 fi
 
 gunicorn -D -b 0.0.0.0:8000 covermonkey:app
-cd ..
+cd -
 echo 'Covermonkey listening on port 8000'
