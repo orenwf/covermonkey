48c9830703cbbb6e1e863276a30ee0303228956c
OrenWF <ofriedman@acm.org>
2018-04-15 20:00:04 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,22 @@
 
 TITLE='[CoverMonkey Setup]:'
 MYPYTHON=python3
+PACKAGEMGR=yum
 
 if lsb_release -d | grep -q 'Ubuntu.*16.04' 
 then
 	MYPYTHON=python3.6
+	PACKAGEMGR=apt
 	echo "$TITLE installing Python3.6 and pip"
 	sudo add-apt-repository ppa:deadsnakes/ppa
-	sudo apt-get update
-	sudo apt-get install python3.6
-	sudo apt-get install python3-pip
+	sudo $PACKAGEMGR install python3.6
 else
 	echo 'Not on Ubuntu 16.04'
+	sudo $PACKAGEMGR install python3 
 fi
+
+sudo $PACKAGEMGR install python3-pip
+pip3 install -U pip --user
 
 if [ ! -d "venv" ]
 then
@@ -22,10 +26,15 @@ else
 	echo 'venv already exists'
 fi
 
+sudo $PACKAGEMGR install python2 python2-pip
+pip2 install -U pip --user
+pip2 install supervisor --user
+
+echo -e "[program:covermonkey]\ncommand=~/venv/bin/gunicorn -b 0.0.0.0:8000 covermonkey:app\ndirectory=~/covermonkey\nuser=$USER\nautostart=true\nautorestart=true\nstopasgroup=true\nkillasgroup=true" > /etc/supervisor/conf.d/covermonkey.conf
+
 source venv/bin/activate
-pip install -U pip
 cd covermonkey
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 export FLASK_APP='covermonkey.py'
 echo "$TITLE set FLASK_APP=$FLASK_APP"
@@ -39,6 +48,5 @@ else
 	echo "$TITLE db file and migrations directory already exist"
 fi
 
-# TODO set up supervisor to run gunicorn
-gunicorn -b 0.0.0.0:8000 covermonkey:app
+supervisorctl reload
 echo 'Covermonkey listening on port 8000'
